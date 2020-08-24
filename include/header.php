0a5a461815f7d5c17fb53f11e39dea1697ccc917
osm313 <69797758+osm313@users.noreply.github.com>
2020-08-24 20:48:29 +0500

    Update header.php

--- include/header.php
+++ include/header.php
@@ -8,7 +8,7 @@ require 'dbconfig.php';
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Welcome to Softech</title>
+    <title>Softech</title>
     
     <link rel="shortcut icon" type="image/png" href="app-assets/img/ico/favicon-32.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
