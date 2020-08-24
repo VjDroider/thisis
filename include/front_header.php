3dddb51ddf1e23fe544e77a9a20288e38939c994
osm313 <69797758+osm313@users.noreply.github.com>
2020-08-24 20:45:23 +0500

    Update front_header.php

--- include/front_header.php
+++ include/front_header.php
@@ -23,7 +23,7 @@ else
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
    	 <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login Page - Fresh Fast Grocery Delivery</title>
+    <title>Login to Softech</title>
     
     <link rel="shortcut icon" type="image/png" href="app-assets/img/ico/favicon-32.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
